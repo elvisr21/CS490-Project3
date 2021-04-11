8d40e16c2efd8908c5811cfecfd0774766888a27
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-04-11 06:21:45 +0000

    setting up deleting and changing rows in the table

--- app.py
+++ app.py
@@ -12,8 +12,22 @@ app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICSTIONS']= False
 
 db=Database(app)
-db.insertComment(creator_id=5,recipe_id=2,comment="comment3")
-db.getComments()
+
+
+#db.insertUser(username="test3",password="password",name="test1")
+#db.deleteUser(user_id=1)
+#db.changeUser(user_id=2,newUsername="newUsername",newName="newName")
+#db.getUsers()
+
+#db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test') 
+#db.deleteRecipe(recipe_id=1)
+#db.changeRecipe(recipe_id=2,newName="testt",newDescription='testt',newIngredients='testt')
+#db.getRecipes()
+ 
+#db.insertComment(creator_id=2,comment="comment",recipe_id=2)  
+#db.deleteComment(comment_id=2)
+#db.changeComment(comment_id=3,newComment='commentt')
+#db.getComments()
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
@@ -23,5 +37,5 @@ def index(filename):
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)), debug=True
 )
--- models.py
+++ models.py
@@ -9,10 +9,10 @@ class Database:
             id = self.db.Column(self.db.Integer, primary_key=True)
             username = self.db.Column(self.db.String(80), unique=True, nullable=False)
             password = self.db.Column(self.db.String(50), unique=False, nullable=False)
+            name=self.db.Column(self.db.String(),unique=False,nullable=False)
             def __repr__(self):
-                return '<User username='+self.username+ ' id='+str(self.id)+ ' >' 
+                return '<User username='+self.username+ ' id='+str(self.id)+' name='+self.name+' >' 
         self.User_Table=User
-        
         class Recipe(self.db.Model):
             id = self.db.Column(self.db.Integer, primary_key=True)
             name=self.db.Column(self.db.String(),nullable=False)
@@ -20,19 +20,19 @@ class Database:
             description=self.db.Column(self.db.String(),nullable=False)
             ingredients=self.db.Column(self.db.String(),nullable=False)
             def __repr__(self):
-                return '<Recipe id='+str(self.id)+ ' name=' + self.name+ ' id='+str(self.id)+' >'
+                return '<Recipe id='+str(self.id)+ ' name=' + self.name+ ' id='+str(self.id)+ ' description='+self.description+' ingredients='+self.ingredients+' >'
         self.Recipe_Table=Recipe
         class Comments(self.db.Model):
-            comment_id=self.db.Column(self.db.Integer,primary_key=True)
+            id=self.db.Column(self.db.Integer,primary_key=True)
             creator_id=self.db.Column(self.db.Integer,self.db.ForeignKey('user.id'),nullable=False)
             recipe_id=self.db.Column(self.db.Integer,self.db.ForeignKey('recipe.id'),nullable=False)
             comment=self.db.Column(self.db.String())
             def __repr__(self):
-                return '<Comment '+'id='+str(self.comment_id)+' creator='+str(self.creator_id)+ ' recipe=' + str(self.recipe_id)+' >'
+                return '<Comment '+'id='+str(self.id)+' creator='+str(self.creator_id)+ ' recipe=' + str(self.recipe_id)+ ' comment='+self.comment+' >'
         self.Comment_Table=Comments
         self.db.create_all()
-    def insertUser(self,username:str,password:str):
-        entry=self.User_Table(username=username,password=password)
+    def insertUser(self,username:str,password:str,name:str):
+        entry=self.User_Table(username=username,password=password,name=name)
         self.db.session.add(entry)
         self.db.session.commit()
         print('User ',entry ," was added to database")
@@ -57,3 +57,33 @@ class Database:
     def getComments(self):
         que= self.Comment_Table.query.all()
         print(que)
+    def deleteUser(self,user_id):
+        self.User_Table.query.filter_by(id=user_id).delete()
+        self.db.session.commit()
+    def deleteRecipe(self,recipe_id):
+        self.Recipe_Table.query.filter_by(id=recipe_id).delete()
+        self.db.session.commit()
+    def deleteComment(self,comment_id):
+        self.Comment_Table.query.filter_by(id=comment_id).delete()
+        self.db.session.commit()
+    def changeUser(self,user_id,newUsername,newName):
+        entry=self.User_Table.query.filter_by(id=user_id).first()
+        if entry.username!=newUsername:
+            entry.username=newUsername 
+        if entry.name!=newName:
+            entry.name=newName
+        self.db.session.commit()
+    def changeComment(self,comment_id,newComment):
+        entry=self.Comment_Table.query.filter_by(id=comment_id).first()
+        if entry.comment!=newComment:
+            entry.comment=newComment 
+        self.db.session.commit()
+    def changeRecipe(self,recipe_id,newName,newDescription,newIngredients):
+        entry=self.Recipe_Table.query.filter_by(id=recipe_id).first()
+        if entry.name!=newName:
+            entry.name=newName
+        if entry.description!=newDescription:
+            entry.description=newDescription
+        if entry.ingredients!=newIngredients:
+            entry.ingredients=newIngredients
+        self.db.session.commit()
