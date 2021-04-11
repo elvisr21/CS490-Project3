ff3765881b4074d1196668c416ffc92fa6cadbc0
ELVIS A RODRIGUEZ <elvisrodriguez0621@gmail.com>
2021-04-10 23:52:01 -0400

    Update app.py
    creating database connection

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 from flask import Flask, send_from_directory
+from models import Database
 
 app = Flask(__name__, static_folder='./build/static')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICSTIONS']= False
 
+
+db=Database(app)
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
