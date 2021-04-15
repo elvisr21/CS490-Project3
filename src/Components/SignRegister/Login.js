d85028166f0f19efbb801ec0fc55b9cd7f62b1f6
EC2 Default User <ec2-user@ip-172-31-21-205.ec2.internal>
2021-04-15 16:39:43 +0000

    Created base of Update

--- src/Components/SignRegister/Login.js
+++ src/Components/SignRegister/Login.js
@@ -1,7 +1,50 @@
-import React, { useState } from 'react';
+import React from 'react';
+import validate from './FormValidation';
+import useForm from './useForm';
+import './Form.css';
 
-export function Login(){
-    return <h1>Login</h1>
-}
+const Login = ({ submitForm }) => {
+  const { handleChange, handleSubmit, account, errors } = useForm(
+    submitForm,
+    validate
+  );
+
+  return (
+    <div className='form-content-right'>
+      <form onSubmit={handleSubmit} className='form' noValidate>
+        <h1>
+          Welcome back! Sign in Below!
+        </h1>
+        <div className='form-inputs'>
+          <label className='form-label'>Username</label>
+          <input
+            className='form-input'
+            type='text'
+            name='name'
+            placeholder='Enter your Username'
+            value={account.username}
+            onChange={handleChange}
+          />
+          {errors.username && <p>{errors.username}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            placeholder='Enter your password'
+            value={account.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        <button className='form-input-btn' type='submit'>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
