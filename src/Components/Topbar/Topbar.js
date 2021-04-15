a9913fafc50f2bfa5389ca1ff478881c3dd151fe
MatthewTreboschi <mt393@njit.edu>
2021-04-15 15:57:24 +0000

    topbar layout added. still needs to run with main

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,6 +1,19 @@
-import './topbar.css'
+import './topbar.css';
 
-
-export const Topbar=()=>{
-    return <h1> Topbar</h1>
-}
+export const Topbar=(logged)=>{
+    return (
+        <div style="background-color: #b3937f">
+            {logged ? (
+                <p class="navig">
+                    <welcome>Welcome, { name }</welcome>
+                    <option href="profile">View Profile</option>
+                </p>
+            ):(
+                <p class="navig">
+                    <option href="login">Login</option>
+                </p>
+            )}
+            <h2 id="logo" style="color: white"><i>Chef Masters</i></h2>
+        </div>
+    );
+};
