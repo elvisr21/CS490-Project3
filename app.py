f92a585e1b7a2f8148e7181365deb217e83ae61f
Naitam Gandhi <ec2-user@ip-172-31-3-245.ec2.internal>
2021-04-18 18:19:47 +0000

    nothing special

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask import Flask, send_from_directory
 from models import Database
 from dotenv import load_dotenv, find_dotenv
 
-app = Flask(__name__, static_folder='./build/static')
 load_dotenv(find_dotenv())
+app = Flask(__name__, static_folder='./build/static')
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -19,21 +19,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICSTIONS']= False
 
 db=Database(app)
 
+db.insertUser(username="test3",password="password",name="test1")
+db.deleteUser(user_id=1)
+db.changeUser(user_id=2,newUsername="newUsername",newName="newName")
+db.getUsers()
 
-#db.insertUser(username="test3",password="password",name="test1")
-#db.deleteUser(user_id=1)
-#db.changeUser(user_id=2,newUsername="newUsername",newName="newName")
-#db.getUsers()
-
-#db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test') 
-#db.deleteRecipe(recipe_id=1)
-#db.changeRecipe(recipe_id=2,newName="testt",newDescription='testt',newIngredients='testt')
-#db.getRecipes()
+db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test') 
+db.deleteRecipe(recipe_id=1)
+db.changeRecipe(recipe_id=2,newName="testt",newDescription='testt',newIngredients='testt')
+db.getRecipes()
  
-#db.insertComment(creator_id=2,comment="comment",recipe_id=2)  
-#db.deleteComment(comment_id=2)
-#db.changeComment(comment_id=3,newComment='commentt')
-#db.getComments()
+db.insertComment(creator_id=2,comment="comment",recipe_id=2)  
+db.deleteComment(comment_id=2)
+db.changeComment(comment_id=3,newComment='commentt')
+db.getComments()
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
