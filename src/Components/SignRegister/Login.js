e74e936fa3a0c7fbdc772e623ba96c263bd4d25c
EC2 Default User <ec2-user@ip-172-31-21-205.ec2.internal>
2021-04-18 18:44:00 +0000

    Login started

--- src/Components/SignRegister/Login.js
+++ src/Components/SignRegister/Login.js
@@ -55,9 +55,6 @@ const Register = ({ submitForm }) => {
         <button className='form-input-btn' type='submit'>
           Sign up
         </button>
-        <span className='form-input-login'>
-          Already have an account? Login <a href='#'>here</a>
-        </span>
       </form>
     </div>
   );
