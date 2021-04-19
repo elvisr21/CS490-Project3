d0f35c1f3aaeef5c33a9828cb9de4ccae29f07a6
MatthewTreboschi <mt393@njit.edu>
2021-04-19 18:01:01 +0000

    update topbar to run smoother

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,4 +1,5 @@
 import './topbar.css';
+import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 
 export const Topbar=()=>{
@@ -7,16 +8,16 @@ export const Topbar=()=>{
     return (
         <div style={{"background-color": "#b3937f"}}>
             {logged ? (
-                <p class="navig">
-                    <a href="/">Home</a>
-                    <welcome>Welcome,</welcome>
-                    <a href="/profile">View Profile</a>
-                    <a href="/newrecipe">New Recipe</a>
+                <p className="navig">
+                    <Link to="/">Home</Link>
+                    <p>Welcome, {name}</p>
+                    <Link to="/profile">View Profile</Link>
+                    <Link to="/newrecipe">New Recipe</Link>
                 </p>
             ):(
-                <p class="navig">
-                    <a href="/">Home</a>
-                    <a href="/signRegister">Login</a>
+                <p className="navig">
+                    <Link to="/">Home</Link>
+                    <Link to="/signRegister">Login</Link>
                 </p>
             )}
             <h2 id="logo"><i>Chef Masters</i></h2>
