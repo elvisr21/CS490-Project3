342c3fcdd3ed3c7b3fde7d4411a242cb32f39519
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-04-19 21:40:59 +0000

    fixed current eslint error on recipe.js

--- src/App.js
+++ src/App.js
@@ -21,11 +21,15 @@ function App() {
       <TopbarComponent id={User.id} />
       <Switch>
         <Route path="/" exact component={MainComponent} />
-        { <Route path="/signRegister" exact component={SignRegisterComponent} />}
+        <Route path="/signRegister" exact component={SignRegisterComponent} />
         <Route path="/favorite" exact component={FavoriteComponent} />
         <Route path="/recipe/:RecipeID" exact component={RecipeComponent} />
         <Route path="/user/:UserID" exact component={UserComponent} />
-        {<Route path="/createRecipe" exact component={() => <CreateRecipeComponent id={User.id} />}/>}
+        <Route
+          path="/createRecipe"
+          exact
+          component={() => <CreateRecipeComponent id={User.id} />}
+        />
       </Switch>
     </Router>
   );
