7443d0873e73e585269dfd552bfb6ec2b7a46214
Naitam Gandhi <ec2-user@ip-172-31-3-245.ec2.internal>
2021-04-19 20:20:36 +0000

    weird merge differences being resolved

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-import './recipe.css'
-import {  useParams } from 'react-router-dom'
-import axios from 'axios'
-import React, {useState,useEffect} from 'react'
-
-export const Recipe=()=>{
-    const [recipe,setRecipe]=useState(undefined)
-    const  id  = useParams()['RecipeID']
-   // const RecipeId= props.params.RecipeID;
-    useEffect(()=>{
-        axios.get('/getRecipebyId', {
-                   params: {
-                        id: id
-                    }
-        }).then(res=>{
-               const data=res['data']
-                Object.entries(data['comments']).map((comment,index)=>{
-                    console.log(comment)
-                })
-               setRecipe({
-                   "name":data["name"],
-                   "creator_id":data['creator_id'],
-                   "creator_name":data['creator_name'],
-                   "cuisine": data['cuisine'],
-                   "description": data['description'],
-                   "ingredients": data['ingredients'],
-                   "img": data['img'],
-                   "comments": data['comments'],
-                   "instructions":data['instructions']
-               })
-        })
-    },[])
-    console.log(recipe);
-    return( 
-        <>
-            { (recipe!==undefined) && 
-                <div className="recipe"> 
-                    <img src={recipe['img']}/>
-                    <div className="Recipe_Name">Name: {recipe["name"]}</div>
-                    <div className="Creator">Creator: {recipe["creator_name"]}</div>
-                    <div className="Creator_id">Creator_id: {recipe["creator_id"]}</div>
-                    <div className="Cuisine">Cuisine: {recipe["cuisine"]}</div>
-                    
-                    <div className="Description">Description: {recipe["description"]}</div>
-                    <div className="Instructions">Instructions:  <br/>
-                        {
-                            recipe['instructions'].map((instruction,index)=>{
-                                return <div className='instruction'>{index+1}. {instruction} </div>
-                            })
-                        }
-                    </div>
-                    <br/>
-                    <div className="Comments">Comments: 
-                        {Object.entries(recipe['comments']).map((comment)=>{
-                            return(
-                                <div className="Comment">
-                                        <div className="Comment_Creator">
-                                            creator={comment[1]['name']} 
-                                        </div>
-                                        <div className="creator_id">
-                                            creator_id={comment[1]['id']} 
-                                        </div>
-                                        <div className="Comment">
-                                            comment={comment[1]['comment']} 
-                                        </div>
-                                        <br/>
-                                </div>
-                            )
-                        })}
-                    </div>
-=======
 import './recipe.css';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -114,7 +42,6 @@ export const Recipe = () => {
           <div className="Creator">Creator: {recipe.creator_name}</div>
           <div className="Creator_id">Creator_id: {recipe.creator_id}</div>
           <div className="Cuisine">Cuisine: {recipe.cuisine}</div>
->>>>>>> cbc6bd0ea9d1566138449a723bf02e1721e95612
 
           <div className="Description">Description: {recipe.description}</div>
           <div className="Instructions">
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,44 +1,5 @@
 import { Topbar } from './Topbar/Topbar';
 import Form from './SignRegister/Form';
-<<<<<<< HEAD
-import {Main} from './Main/Main';
-import {Favorite} from './Favorite/Favorite';
-import {Recipe} from './Recipe/Recipe';
-import {User} from './User/User';
-import {CreateRecipe} from './CreateRecipe/CreateRecipe';
-
-export const TopbarComponent=()=>{
-    return <Topbar/>
-}
-export const SignRegisterComponent=()=>{
-    return <Form/>
-}
-export const MainComponent=()=>{
-    return(
-            <Main/>    
-    )
-}
-export const FavoriteComponent=()=>{
-    return(
-        <Favorite/>   
-    )
-}
-export const RecipeComponent=()=>{
-    return(
-        <Recipe/>
-    )
-}
-export const UserComponent=()=>{
-    return(
-        <User/> 
-    )
-}
-export const CreateRecipeComponent=(props)=>{
-    return(
-        <CreateRecipe id={props.id}/>
-    )
-}
-=======
 import { Main } from './Main/Main';
 import { Favorite } from './Favorite/Favorite';
 import { Recipe } from './Recipe/Recipe';
@@ -52,4 +13,3 @@ export const FavoriteComponent = () => <Favorite />;
 export const RecipeComponent = () => <Recipe />;
 export const UserComponent = () => <User />;
 export const CreateRecipeComponent = (props) => <CreateRecipe id={props.id} />;
->>>>>>> cbc6bd0ea9d1566138449a723bf02e1721e95612
