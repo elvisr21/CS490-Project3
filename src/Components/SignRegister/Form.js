63985451acd23e88bfbd1399635f9871c76fc0d0
Naitam Gandhi <ec2-user@ip-172-31-3-245.ec2.internal>
2021-04-19 20:23:04 +0000

    weird merge differences being resolved

--- src/Components/SignRegister/Form.js
+++ src/Components/SignRegister/Form.js
@@ -12,25 +12,16 @@ const Form = () => {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className='form-container'>
-        <a className='close-btn' href='/'>X</a>
-        <div className='form-content-left'>
-          <img className='form-img' src='images/register-logo.jpg' alt='register-logo' />
-        </div>
-        {!isSubmitted ? (
-          <Register submitForm={submitForm}/>
-        ) : (
-          <FormSuccess href='/'/>
-        )}
-=======
       <div className="form-container">
         <span className="close-btn">×</span>
         <div className="form-content-left">
           <img className="form-img" src="images/register-logo.jpg" alt="register-logo" />
         </div>
-        {!isSubmitted ? <Register submitForm={submitForm} /> : <FormSuccess />}
->>>>>>> cbc6bd0ea9d1566138449a723bf02e1721e95612
+        {!isSubmitted ? 
+        <Register submitForm={submitForm} /> 
+        : 
+        <FormSuccess />
+        }
       </div>
     </>
   );
