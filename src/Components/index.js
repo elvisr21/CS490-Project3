ad67d2b05de7318d48c40cc61c4e9b0e2d24c5a5
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-04-19 20:54:29 +0000

    linting fixes

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Topbar } from './Topbar/Topbar';
 import Form from './SignRegister/Form';
 import { Main } from './Main/Main';
@@ -6,10 +8,10 @@ import { Recipe } from './Recipe/Recipe';
 import { User } from './User/User';
 import { CreateRecipe } from './CreateRecipe/CreateRecipe';
 
-export const TopbarComponent = () => <Topbar />;
-export const SignRegisterComponent = () => <Form />;
-export const MainComponent = () => <Main />;
-export const FavoriteComponent = () => <Favorite />;
-export const RecipeComponent = () => <Recipe />;
-export const UserComponent = () => <User />;
+export const TopbarComponent: React.FunctionComponent = (props) => () => <Topbar id={props.id} />;
+export const SignRegisterComponent = (props) => <Form />;
+export const MainComponent = (props) => <Main />;
+export const FavoriteComponent = (props) => <Favorite />;
+export const RecipeComponent = (props) => <Recipe />;
+export const UserComponent = (props) => <User />;
 export const CreateRecipeComponent = (props) => <CreateRecipe id={props.id} />;
