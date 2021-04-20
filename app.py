00f7b2e4e1bd0dd36822c4bd67005cbab106638e
Naitam Gandhi <ec2-user@ip-172-31-3-245.ec2.internal>
2021-04-20 01:20:44 +0000

    added formating to the python files

--- app.py
+++ app.py
@@ -10,21 +10,18 @@ from models import Database
 from dotenv import load_dotenv, find_dotenv
 import hashlib
 
-
 app = Flask(__name__, static_folder='./build/static')
 load_dotenv(find_dotenv())
 
-
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICSTIONS']= False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICSTIONS'] = False
 
 #used to salt hashed passwords
-salt='cs490project3'
+salt = 'cs490project3'
 
-db=Database(app)
-
+db = Database(app)
 '''
 register endpoint
 takes json 
@@ -34,17 +31,21 @@ takes json
     'name':'name'
 }
 '''
-@app.route('/register',methods=['POST'])
+
+
+@app.route('/register', methods=['POST'])
 def register():
     data = json.loads(request.data.decode())
     print(data)
-    return registerUser(username=data['username'],password=data['password'],name=data['name'])
-    
-    
-def registerUser(username,password,name):
-    hashedpassword=hashlib.md5((password+salt).encode()).hexdigest()
-    return db.insertUser(username=username,password=hashedpassword,name=name)
-    
+    return registerUser(username=data['username'],
+                        password=data['password'],
+                        name=data['name'])
+
+
+def registerUser(username, password, name):
+    hashedpassword = hashlib.md5((password + salt).encode()).hexdigest()
+    return db.insertUser(username=username, password=hashedpassword, name=name)
+
 
 '''
 login endpoint 
@@ -54,17 +55,18 @@ takes json
     'password':'test'
 }
 '''
-@app.route('/login',methods=["GET","POST"])
+
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
     data = json.loads(request.data.decode())
-    return loginUser(username=data['username'],password=data['password'])
-    
-    
-def loginUser(username,password):
-    hashedpassword=hashlib.md5((password+salt).encode()).hexdigest()
-    return db.User_Exist(username=username,password=hashedpassword)
+    return loginUser(username=data['username'], password=data['password'])
 
-    
+
+def loginUser(username, password):
+    hashedpassword = hashlib.md5((password + salt).encode()).hexdigest()
+    return db.User_Exist(username=username, password=hashedpassword)
+
 
 '''
     endpoint to add new recipes
@@ -86,12 +88,12 @@ def loginUser(username,password):
 # db.changeUser(user_id=2,newUsername="newUsername",newName="newName")
 # db.getUsers()
 
-# db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test') 
+# db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test')
 # db.deleteRecipe(recipe_id=1)
 # db.changeRecipe(recipe_id=2,newName="testt",newDescription='testt',newIngredients='testt')
 # db.getRecipes()
- 
-# db.insertComment(creator_id=2,comment="comment",recipe_id=2)  
+
+# db.insertComment(creator_id=2,comment="comment",recipe_id=2)
 # db.deleteComment(comment_id=2)
 # db.changeComment(comment_id=3,newComment='commentt')
 # db.getComments()
@@ -101,20 +103,28 @@ def loginUser(username,password):
 # db.changeUser(user_id=2,newUsername="newUsername",newName="newName")
 # db.getUsers()
 
-# db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test') 
+# db.insertRecipe(name='test2',creator_id=3,description='test',ingredients='test')
 # db.deleteRecipe(recipe_id=1)
 # db.changeRecipe(recipe_id=2,newName="testt",newDescription='testt',newIngredients='testt')
 # db.getRecipes()
- 
-# db.insertComment(creator_id=2,comment="comment",recipe_id=2)  
+
+# db.insertComment(creator_id=2,comment="comment",recipe_id=2)
 # db.deleteComment(comment_id=2)
 # db.changeComment(comment_id=3,newComment='commentt')
 # db.getComments()
 
-@app.route("/AddRecipe",methods=["POST"])
+
+@app.route("/AddRecipe", methods=["POST"])
 def addRecipe():
-    data=json.loads(request.data.decode())
-    return db.insertRecipe(name=data['name'],creator_id=data['id'],description=data['description'],ingredients=json.dumps(data['Ingredients']),cuisine=data['cuisine'],img=data['image'],instructions=json.dumps(data['Instructions']))
+    data = json.loads(request.data.decode())
+    return db.insertRecipe(name=data['name'],
+                           creator_id=data['id'],
+                           description=data['description'],
+                           ingredients=json.dumps(data['Ingredients']),
+                           cuisine=data['cuisine'],
+                           img=data['image'],
+                           instructions=json.dumps(data['Instructions']))
+
 
 '''
     endpoint for getting recipies for id for recipe page
@@ -128,28 +138,29 @@ def addRecipe():
     }
     id being recipe id
 '''
-@app.route('/getRecipebyId',methods=["GET"])
+
+
+@app.route('/getRecipebyId', methods=["GET"])
 def getRecipeByID():
-    Recipe_ID=request.args.get('id')
+    Recipe_ID = request.args.get('id')
     return db.getRecipesById(Recipe_ID)
 
 
-
-
-@app.route('/GetRecipesbyCuisine',methods=["GET"])
+@app.route('/GetRecipesbyCuisine', methods=["GET"])
 def getRecipes():
-    cuisine=request.args.get('cuisine')
+    cuisine = request.args.get('cuisine')
     #data = json.loads(request.data.decode())
-    return getRecipesbyCuisine(data['cuisine'],data['recipe_limit'])
-    
-    
-def getRecipesbyCuisine(cuisine:str,recipe_limit:int):
-    returnval= db.getRecipesbyCuisine(cuisine,recipe_limit)
+    return getRecipesbyCuisine(data['cuisine'], data['recipe_limit'])
+
+
+def getRecipesbyCuisine(cuisine: str, recipe_limit: int):
+    returnval = db.getRecipesbyCuisine(cuisine, recipe_limit)
     print(returnval)
     return returnval
 
+
 #id 1
-#print(registerUser("taco","taco","taco")) 
+#print(registerUser("taco","taco","taco"))
 #print(loginUser("taco4","taco"))
 #print(getRecipesbyCuisine("chinese",0))
 #db.getRecipes()
@@ -158,14 +169,13 @@ def getRecipesbyCuisine(cuisine:str,recipe_limit:int):
 #db.changeUser(user_id=2,newUsername="newUsername",newName="newName")
 #db.getUsers()
 
-
-#db.insertRecipe(name='test2',creator_id=1,description='test',ingredients='test',cuisine="chinese", img='test', instructions="test#") 
+#db.insertRecipe(name='test2',creator_id=1,description='test',ingredients='test',cuisine="chinese", img='test', instructions="test#")
 #db.deleteRecipe(recipe_id=1)
 #db.changeRecipe(recipe_id=2,newName="testt",newDescription='testt',newIngredients='testt')
 #db.getRecipes()
 
 
-#db.insertComment(creator_id=1,comment="comment",recipe_id=6)         
+#db.insertComment(creator_id=1,comment="comment",recipe_id=6)
 #db.deleteComment(comment_id=2)
 #db.changeComment(comment_id=3,newComment='commentt')
 #db.getComments()
@@ -173,7 +183,8 @@ def getRecipesbyCuisine(cuisine:str,recipe_limit:int):
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)), debug=True
-)
+
+
+app.run(host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+        debug=True)
