3cd4967d79871a89a601ed8717c20af352be9b07
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-04-28 20:57:25 +0000

    ingredients and amount shows now

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -29,6 +29,7 @@ const Recipe: React.FunctionComponent = () => {
         });
       });
   }, []);
+  console.log(recipe)
   return (
     <>
       {recipe !== undefined && (
@@ -40,11 +41,24 @@ const Recipe: React.FunctionComponent = () => {
           <div className="Cuisine">Cuisine: {recipe.cuisine}</div>
 
           <div className="Description">Description: {recipe.description}</div>
-          <div className="Instructions">
+          <div className="ingredients_recipe">
+              Ingredients: <br/>
+              {
+                recipe.ingredients.map((ingredient,index)=>{
+                  return( 
+                  <div className="ingredient_recipe">
+                      <div className="ingredient_name_recipe">{ingredient['name']}</div>
+                      <div className="ingredient_amount_recipe">{ingredient['amount']}</div>
+                  </div>
+                  )
+                })
+              }
+          </div>
+          <div className="instructions_recipe">
             Instructions: <br />
             {recipe.instructions.map((instruction, index) => (
-              <div className="instruction">
-                {index + 1}. {instruction}{' '}
+              <div className="instruction_recipe">
+                {index + 1}. {instruction}
               </div>
             ))}
           </div>
