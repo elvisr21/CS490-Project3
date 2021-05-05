9709ba9be77adb50a06ee1c7cc710b199787b585
Naitam Gandhi <ec2-user@ip-172-31-3-245.ec2.internal>
2021-05-05 02:26:43 +0000

    adding landing page

--- src/Components/LandingPage/Landingpage.js
+++ src/Components/LandingPage/Landingpage.js
@@ -1,3 +1,23 @@
 import React from 'react';
+import './Landingpage.css';
 
-export const Landing = () => <h1>Landing</h1>;
+export const Landing = () => {
+    
+    return (
+        <body>
+            <div class="header">
+                <h1>Welcome to Chef Masters!</h1>
+                <p>This is a recipe sharing website created by Naitam, Mike, Elvis, and Matt.</p>
+                <p>(1) what the functionality is, (2) why it matters, and (3) who it is made by</p>
+            </div>
+            <div class="body">
+                <h1>What does this app offer?</h1>
+                <p>This app offers a platform to share your culinary skills online in a community full of people with similar goals. You can add your own recipes, view comments and feedback from others, and see other people's creations too.</p>
+            </div>
+            <div class="body">
+                <h1>Why does this app matter?</h1>
+                <p>Since quarantine has hit majority of the world quite hard, this app lets you share ideas and quickly turn your boredom into a blissfull meal.</p>
+            </div>
+        </body>
+    )
+};
